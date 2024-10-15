424e9406316962fe7d947218efbfe622097c9f0d
rachmadrf <rachmad17845@gmail.com>
2024-10-15 20:37:23 +0700

    upload SiakadFor21_1

---
+++ SiakadFor21_1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SiakadFor21_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        double nilai = 0, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        }
+        
+        if (nilai >= 60) {
+            lulus++;
+        } else {
+            tidakLulus++;
+        }
+        
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
+    }
+}
